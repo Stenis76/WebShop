d09a156494531a0fa09a8fc1f6b000f0bdfb6f49
FThelin <fredrikthelin82@gmail.com>
2020-03-19 14:44:07 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AppBar from "./components/appbar";
+import {
+  Box,
+  Button,
+  Collapsible,
+  Heading,
+  Grommet,
+  Layer,
+  ResponsiveContext
+} from "grommet";
+import { FormClose, Cart } from "grommet-icons";
+
+const theme = {
+  global: {
+    colors: {
+      brand: "#49516f"
+    },
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px"
+    }
+  }
+};
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState<boolean>(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme} full>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar>
+              <Heading level="3" margin="none">
+                My App
+              </Heading>
+              <Button
+                icon={<Cart />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
+            <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+              <Box flex align="center" justify="center">
+                app body
+              </Box>
+              {!showSidebar || size !== "small" ? (
+                <Collapsible direction="horizontal" open={showSidebar}>
+                  <Box
+                    flex
+                    width="medium"
+                    background="light-2"
+                    elevation="small"
+                    align="center"
+                    justify="center"
+                  >
+                    sidebar
+                  </Box>
+                </Collapsible>
+              ) : (
+                <Layer>
+                  <Box
+                    background="light-2"
+                    tag="header"
+                    justify="end"
+                    align="center"
+                    direction="row"
+                  >
+                    <Button
+                      icon={<FormClose />}
+                      onClick={() => setShowSidebar(false)}
+                    />
+                  </Box>
+                  <Box
+                    fill
+                    background="light-2"
+                    align="center"
+                    justify="center"
+                  >
+                    sidebar
+                  </Box>
+                </Layer>
+              )}
+            </Box>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
   );
 }
 
---
+++ src/components/appbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box } from "grommet";
+
+interface Iprops {}
+
+const AppBar = (props: Iprops) => {
+  return (
+    <Box
+      tag="header"
+      direction="row"
+      align="center"
+      justify="between"
+      background="brand"
+      pad={{ left: "medium", right: "small", vertical: "small" }}
+      elevation="medium"
+      style={{ zIndex: 1 }}
+      {...props}
+    />
+  );
+};
+
+export default AppBar;
