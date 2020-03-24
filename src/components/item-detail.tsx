d67c644ffab2dc977e1507cd1dd441efd32c688d
Stenis76 <stenqvist.dick@gmail.com>
2020-03-24 11:46:45 +0100

    detalid items
    Co-authored-by: FThelin <fredrikthelin82@gmail.com>

--- src/components/item-detail.tsx
+++ src/components/item-detail.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+m viewimport React from "react";
+import { Box, Text, Layer, Heading, Image } from "grommet";
 
-const ItemDetails = () => <div></div>;
+interface Iprops {
+  item: any;
+}
+
+const ItemDetails = (props: Iprops) => {
+  console.log(props.item);
+  return (
+    <Box
+      width="large"
+      height="large"
+      round="none"
+      align="center"
+      justify="center"
+      background="light-3"
+    >
+      <Heading>{props.item.name}</Heading>
+      <Text>size{props.item.size}</Text>
+      <Image src={props.item.imageUrl} />;<Text>pris{props.item.price}</Text>
+    </Box>
+  );
+};
 
 export default ItemDetails;
--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import { Box } from "grommet";
+import { Box, Button, Layer } from "grommet";
 import CartContext from "../contexts/cartContext/context";
+import ItemDetails from "../components/item-detail";
 
 interface Iprops {
   item: any;
@@ -9,6 +10,7 @@ interface Iprops {
 const Item = (props: Iprops) => {
   const { addItemToCart } = useContext(CartContext);
   const url = `url(${props.item.imageUrl})`;
+  const [show, setShow] = React.useState(false);
   return (
     <Box
       width="small"
@@ -22,6 +24,17 @@ const Item = (props: Iprops) => {
       <h2>{props.item.name}</h2>
       <span>{props.item.price * 10} kr</span>
       <button onClick={() => addItemToCart(props.item)}>Add to cart</button>
+
+      <Button label="show" onClick={() => setShow(true)} />
+      {show && (
+        <Layer
+          onEsc={() => setShow(false)}
+          onClickOutside={() => setShow(false)}
+        >
+          <ItemDetails item={props.item} />
+          <Button label="close" onClick={() => setShow(false)} />
+        </Layer>
+      )}
     </Box>
   );
 };
