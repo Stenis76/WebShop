f18b41e9191414744619bb2fe457104e853b77d6
Stenis76 <stenqvist.dick@gmail.com>
2020-03-31 15:34:56 +0200

    responsiv meny

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Nav, Text, Box } from "grommet";
+import { Nav, Text, Box, ResponsiveContext, Menu } from "grommet";
 
-const Menu = () => {
+const MyMenu = () => {
   return (
     <Box
       height="5vh"
@@ -12,33 +12,50 @@ const Menu = () => {
       elevation="xsmall"
       pad="small"
     >
-      <Nav direction="row" background="mainText">
-        <Text margin={{ left: "small" }} size="medium">
-          <Link className="link" to="/shop/mens">
-            MENS
-          </Link>
-        </Text>
-        <Text size="medium">
-          <Link className="link" to="/shop/womens">
-            WOMENS
-          </Link>
-        </Text>
-        <Text size="medium">
-          <Link className="link" to="/shop/hats">
-            HATS
-          </Link>
-        </Text>
-        <Text size="medium">
-          <Link className="link" to="/shop/jackets">
-            JACKETS
-          </Link>
-        </Text>
-        <Text size="medium">
-          <Link className="link" to="/shop/sneakers">
-            SNEAKERS
-          </Link>
-        </Text>
-      </Nav>
+      <ResponsiveContext.Consumer>
+        {responsive =>
+          responsive === "small" ? (
+            <Menu
+              label="Menu"
+              items={[
+                { label: "Mens", onClick: () => {} },
+                { label: "Womens", onClick: () => {} },
+                { label: "Hats", onClick: () => {} },
+                { label: "Jackets", onClick: () => {} },
+                { label: "Sneakers", onClick: () => {} }
+              ]}
+            />
+          ) : (
+            <Nav direction="row" background="mainText">
+              <Text margin={{ left: "small" }} size="medium">
+                <Link className="link" to="/shop/mens">
+                  MENS
+                </Link>
+              </Text>
+              <Text size="medium">
+                <Link className="link" to="/shop/womens">
+                  WOMENS
+                </Link>
+              </Text>
+              <Text size="medium">
+                <Link className="link" to="/shop/hats">
+                  HATS
+                </Link>
+              </Text>
+              <Text size="medium">
+                <Link className="link" to="/shop/jackets">
+                  JACKETS
+                </Link>
+              </Text>
+              <Text size="medium">
+                <Link className="link" to="/shop/sneakers">
+                  SNEAKERS
+                </Link>
+              </Text>
+            </Nav>
+          )
+        }
+      </ResponsiveContext.Consumer>
       <Text size="medium">
         <Link className="link" to="/sign-in-sign-up">
           Sign in / Register
@@ -48,4 +65,4 @@ const Menu = () => {
   );
 };
 
-export default Menu;
+export default MyMenu;
