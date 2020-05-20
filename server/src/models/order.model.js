94cfa559da2e74c854a3523b7502c193b6b3003f
Fluffinator97 <philip.arvidsson97@gmail.com>
2020-05-20 08:28:49 +0200

    Before liveshare

---
+++ server/src/models/order.model.js
@@ -0,0 +1,32 @@
+Order-model
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  productId: {
+    type: Array,
+    required: true,
+    trim: true,
+  },
+  freightId: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  activeOrder: {
+    type: Boolean,
+    required: true,
+    trim: true,
+  },
+});
+
+var Order = mongoose.model("Order", OrderSchema);
+module.exports = Order;
