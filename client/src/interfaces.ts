d67812e13661286ee3f53ca03a4b9c4b2a2902ec
Stenis76 <stenqvist.dick@gmail.com>
2020-05-29 10:53:56 +0200

    morgonens förändringar

--- client/src/interfaces.ts
+++ client/src/interfaces.ts
@@ -1,24 +1,23 @@
-
 export interface CollectionItem {
-    id: number;
-    name: string;
-    imageUrl: string;
-    price: number;
-    category: string;
-    season: string[];
-    inventory: {
-        small: string;
-        medium: string;
-        large: string;
-        xlarge: string;
-    }
-    description: string;
-  }
-  
- // behåll till kategorier
-  export interface Collection {
-    id: number;
-    title: string;
-    routeName: string;
-    items: CollectionItem[];
-  }
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: number;
+  category: string;
+  season: string[];
+  inventory: {
+    small: string;
+    medium: string;
+    large: string;
+    xlarge: string;
+  };
+  description: string;
+}
+
+// behåll till kategorier
+export interface Collection {
+  id: number;
+  title: string;
+  routeName: string;
+  items: CollectionItem[];
+}
